577f796999de008c499e4b44e452ad653af47777
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-13 11:56:49 -0700

    Create user controller and specs

--- goal_setter/app/controllers/users_controller.rb
+++ goal_setter/app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user
+      render :show
+    else
+      flash[:errors] = ['User could not be found']
+      redirect_to new_user_url
+    end 
+  end
+  
+  def new
+    @user = User.new
+    render :new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login_user(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new, status: 422
+    end 
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- goal_setter/spec/controllers/users_controller_spec.rb
+++ goal_setter/spec/controllers/users_controller_spec.rb
@@ -14,19 +14,20 @@ RSpec.describe UsersController, type: :controller do
       it "logs in the user" do
         post :create, params: {user: { email: 'email@email.com', password: '123456' }}
         user = User.find_by(email: 'email@email.com')
-        expect(session[session_token]).to eq(user.session_token)
+        expect(session[:session_token]).to eq(user.session_token)
       end
       
       it "redirects to users show page" do
         post :create, params: {user: { email: 'email@email.com', password: '123456' }}
-        expect(response).to render_template(user_path(user))
+        user = User.find_by(email: 'email@email.com')
+        expect(response).to redirect_to(user_url(user))
       end
     end
     
     context 'with invalid params' do
       it 'validates presense of password and renders template with appropriate errors' do
         post :create, params: {user: { email: 'email@email.com', password: '123' }}
-        expect(response).to render_template(new_user)
+        expect(response).to render_template(:new)
         expect(flash[:errors]).to_not be_nil
       end
     end
